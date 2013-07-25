b05a97969e73362252379ea3b1d3b1c4f0b8cdec
yaroslav <yaroslav@yaroslav-VirtualBox.(none)>
2013-07-26 00:27:50 +0300

    update

--- src/com/itogo/MainActivity.java
+++ src/com/itogo/MainActivity.java
@@ -4,6 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
